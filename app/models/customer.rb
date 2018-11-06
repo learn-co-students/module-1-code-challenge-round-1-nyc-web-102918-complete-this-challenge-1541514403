1c1a51b24f965f3cdb4129544dc8f530387aedcb
demiansims <demiansims@gmail.com>
2018-11-06 10:38:43 -0500

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,31 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+   @@all = [] #create an array to access all customrers
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+end
+
+ def add_review(restaurant, content, rating) #initializes a new review within customer class
+   Review.new(rating, content, self, restaurant)
+ end
+
+ def num_reviews #iterates through Review class which connects restaurants with customer
+
+   customer_reviews = Review.all.select { |review| review.author == self }
+   customer_reviews.length #number of arrays
+  end
+
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,22 @@
 class Review
-  
-end
 
+  attr_accessor :rating, :content
+  attr_reader :author, :restaurant
+
+  @@all = []
+
+  def initialize(rating, content, author, restaurant) #initializes a review with all attributes
+    @rating = rating
+    @content = content
+    @author = author
+    @restaurant = restaurant
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  
+
+end
