c9a72bce4409dcd3072713c8f8a5f87c1d40dc66
hsterman <heather.sterman531@gmail.com>
2018-11-06 11:08:11 -0500

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,16 @@
 class Restaurant
-  attr_accessor :name
+  attr_accessor :name #this means it can't be changed 
+  @@all = [] #all the restaurants are stored in this array
 
   def initialize(name)
+    #everything in here is equal to itself
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    #returns all restaurant instances
+    @@all
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,9 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+rev1 = Review.new("good")
+rev2 = Review.new("ok")  # these are supposed to print out in the console. they don't work, because i get an undefined method of tools when i type ruby tools/console.rb. but if they were to work, this would be what the code would look like (i think).
+rev3 = Review.new("ugh")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
