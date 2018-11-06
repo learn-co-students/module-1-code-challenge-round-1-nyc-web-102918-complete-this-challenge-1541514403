c1d07c2518e05d02d47c8e1da004e17f8a3454c6
vibhu mahendru <vibhumahendru@vibhus-MacBook-Pro.local>
2018-11-06 10:34:54 -0500

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,22 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+chic = Restaurant.new("Chic")
+dominos = Restaurant.new("Dominos")
+
+
+
+vibhu = Customer.new("vibhu", "mahendru")
+bharat = Customer.new("aash", "arora")
+aash = Customer.new("aash", "tiku")
+
+review1 = Review.new(vibhu, chic, 4, "blah blah blah")
+review2 = Review.new(bharat, dominos, 5, "kokokoko")
+
+vibhu.add_review(dominos, 5, "hahahahahddddddddddd")
+aash.add_review(dominos, 3, "lololol")
+aash.add_review(dominos, 5, "loljjjgfdfdl")
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
