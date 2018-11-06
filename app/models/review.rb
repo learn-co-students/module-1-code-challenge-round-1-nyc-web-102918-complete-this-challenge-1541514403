1e4151abc30bf28fb4e21601ebd299c4dfca24ca
digitopolis <matt.readout@gmail.com>
2018-11-06 10:27:15 -0500

    completed code challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,29 @@
 class Review
-  
-end
+                # allow customers to edit their reviews
+  attr_accessor :rating, :content
+  attr_reader :customer, :restaurant
 
+  @@all = []
+
+  def initialize(customer, restaurant, rating, content)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    if rating <= 1  # restrict rating to 1-5
+      @rating = 1
+    elsif rating >= 5
+      @rating = 5
+    else
+      @rating = rating
+    end
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,27 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+matt_r = Customer.new("Matt", "Readout")
+matt_f = Customer.new("Matt", "Fonda")
+jennie = Customer.new("Jennie", "Griswold")
+mike = Customer.new("Mike", "Cheng")
+david = Customer.new("David", "Readout")
+
+cafe_z = Restaurant.new("Cafe Zona Sur")
+diner = Restaurant.new("Sunset Park Diner")
+slimak = Restaurant.new("Slimak")
+parlay = Restaurant.new("Parlay")
+
+matt_r.add_review(diner, "great donuts!", 4)
+mike.add_review(slimak, "i hate this place :(", 0)
+jennie.add_review(cafe_z, "omg, like everything is so good here! i love the burgers and fries especially.", 7)
+david.add_review(diner, "meh, it's ok", 3)
+mike.add_review(diner, "found a hair in my eggs", 1)
+david.add_review(slimak, "grat coffee!", 5)
+mike.add_review(slimak, "i love this place now!", 5)
+matt_f.add_review(cafe_z, "it's pretty good", 3)
+matt_r.add_review(cafe_z, "the food is good, but they close early", 4)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
