a52317e455a7216869e9878f7b0df7027396308f
jbmenashi <jbmenashi@gmail.com>
2018-11-06 10:16:46 -0500

    all set

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,60 @@
 class Customer
+
   attr_accessor :first_name, :last_name
+
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def reviews # goes through all the reviews, picks out the matching customer ones
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+  def restaurants # goes through the matched reviews and grabs the restaurant, uniq for no repeats
+    restaurants = reviews.map do |review|
+      review.restaurant
+    end
+    restaurants.uniq
+  end
+
+  def self.find_by_name(name) # matches the customer name to the argument name, returns just the first one
+    self.all.find do |customer|
+      customer.full_name == name
+    end
+  end
+
+  def self.find_all_by_first_name(first_name) # same as above except its an array of all the names not just the first
+    self.all.select do |customer|
+      customer.first_name == first_name
+    end
+  end
+
+  def self.all_names # maps a new array of all the names of customers
+    self.all.map do |customer|
+      customer.full_name
+    end
+  end
+
+  def add_review(restaurant, rating, content) # self is the customer instance youre calling the method on, the rest you just fill in
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def num_reviews # the array of reviews already exists, just find the length of the array
+    reviews.length
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,21 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+c1 = Customer.new("Bob", "Smith")
+c2 = Customer.new("Tim", "Jones")
+c3 = Customer.new("Bob", "Lewis")
+
+res1 = Restaurant.new("Marios")
+res2 = Restaurant.new("Harrys")
+res3 = Restaurant.new("Pauls")
+
+rev1 = Review.new(c1, res1, 3, "Good")
+rev2 = Review.new(c2, res1, 4, "Great")
+rev3 = Review.new(c1, res2, 2, "Poor")
+rev4 = Review.new(c1, res3, 5, "Awesome")
+rev5 = Review.new(c3, res1, 3, "Good")
+rev6 = Review.new(c1, res1, 2, "Really Really Poor")
+rev7 = Review.new(c2, res3, 4, "Super Great")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
