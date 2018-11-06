ca9f838f8317590011aad0b3099b7a7baa856925
EPICZEUS <s77broz@aol.com>
2018-11-06 09:52:00 -0500

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,35 @@
 class Restaurant
+	@@all = []
+
+	def self.all
+		@@all
+	end
+
+	def self.find_by_name(name)
+		self.all.find{|restuarant| restaurant.name == name}
+	end
+
   attr_accessor :name
 
   def initialize(name)
     @name = name
+
+    @@all << self
   end
 
+  def reviews
+  	Review.all.select{|review| review.restaurant == self }
+  end
+
+  def customers
+  	reviews.map{|review| review.customer }.uniq
+  end
+
+  def average_star_rating
+  	reviews.map{|review| review.rating }.reduce(:+) / reviews.length.to_f
+  end
+
+  def longest_review
+  	reviews.max_by{|review| review.content.length }
+  end
 end
